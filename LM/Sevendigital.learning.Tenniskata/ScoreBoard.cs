7af16329d7528c20101b56cb78b88edcf7f89a4b
Alex Graff <alex.graff@7digital.com>
2014-07-28 12:22:22 +0100

    End of first session.

---
+++ LM/Sevendigital.learning.Tenniskata/ScoreBoard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sevendigital.learning.Tenniskata
+{
+	class ScoreBoard
+	{
+		public string Display()
+		{
+			return "love:love";
+		}
+
+	}
+}
