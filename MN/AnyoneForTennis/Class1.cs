3f18d3f71543bcb153011f3bdb60060737be6f20
Alex Graff <alex.graff@7digital.com>
2014-07-28 12:23:46 +0100

    End of first session.

---
+++ MN/AnyoneForTennis/Class1.cs
@@ -0,0 +1,75 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AnyoneForTennis
+{
+
+    [TestFixture]
+    public class MatchTests
+    {
+        [Test]
+        public void RenameMe()
+        {
+            var scoreBoard = new ScoreBoard();
+            WinMatch(scoreBoard, Player.One);
+            scoreBoard.GetWinner().Should().Be(Player.One);
+        }
+
+        [Test]
+        public void PlayerTwoWins()
+        {
+            var scoreBoard = new ScoreBoard();
+            WinMatch(scoreBoard, Player.Two);
+            scoreBoard.GetWinner().Should().Be(Player.Two);
+        }
+
+        private static void WinMatch(ScoreBoard scoreBoard,  Player player)
+        {
+            WinSet(scoreBoard, player);
+            WinSet(scoreBoard, player);
+        }
+
+        private static void WinSet(ScoreBoard scoreBoard, Player player)
+        {
+            for (var i = 0; i < 6; i++)
+            {
+                WinGame(scoreBoard, player);
+            }
+        }
+
+        private static void WinGame(ScoreBoard scoreBoard, Player player)
+        {
+            for (var i = 0; i < 4; i++)
+                scoreBoard.AddPoint(player);
+        }
+    }
+
+    [TestFixture]
+    public class ScoreBoardTests
+    {
+        [Test]
+        public void TestTheBoard()
+        {
+            1.Should().NotBe(2);
+        }
+    }
+
+    public class ScoreBoard 
+    {
+        public Player GetWinner()
+        {
+            return Player.One;
+        }
+
+        public void AddPoint(Player two)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public enum Player
+    {
+        One,
+        Two
+    }
+}
