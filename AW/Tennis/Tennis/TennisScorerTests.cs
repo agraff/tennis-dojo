e470eac102b0a0f04c620fd5c4fe415548473305
Alex Graff <alex.graff@7digital.com>
2014-08-01 16:22:33 +0100

    End of first session.

---
+++ AW/Tennis/Tennis/TennisScorerTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Tennis
+{
+	[TestFixture]
+	public class TennisScorerTests
+	{
+		private string _runningScore;
+		private TennisScorer _tennisScorer;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_tennisScorer = new TennisScorer();
+		}
+
+		[Test]
+		public void Scores_fifteen_love_if_player_one_scores_one_point_and_player_two_scores_none()
+		{
+			_runningScore = _tennisScorer.RunningScoreFor(1,0);
+			Assert.That(_runningScore, Is.EqualTo("Fifteen love"));
+		}
+
+		[Test]
+		public void Scores_thirty_fifteen_if_player_one_scores_two_points_and_player_two_scores_one()
+		{
+			_runningScore = _tennisScorer.RunningScoreFor(2, 0);
+			Assert.That(_runningScore, Is.EqualTo("Thirty love"));
+		}
+
+		[Test]
+		public void Scores_forty_thirty_if_player_one_scores_three_points_and_player_two_scores_two()
+		{
+			_runningScore = _tennisScorer.RunningScoreFor(3, 0);
+			Assert.That(_runningScore, Is.EqualTo("Forty love"));
+		}
+	}
+
+	public class TennisScorer
+	{
+		public string RunningScoreFor(int playerOneScore, int playerTwoScore)
+		{
+			var scoreNames = new Dictionary<int, string>
+				{
+					{ 0, "Love" },
+					{ 1, "Fifteen" },
+					{ 2, "Thirty" },
+					{ 3, "Forty" },
+				};
+
+			return scoreNames[playerOneScore] + " " + scoreNames[playerTwoScore].ToLower();
+		}
+	}
+}
